50d68a38a6cf3cbb89d32ee2059b0d65a48e5385
Hannah Harrisonn <hannah@harrisonn.dev>
2026-04-03 19:59:06 -0300

    fix(textobjects): use positional block matches

--- queries/aura/textobjects.scm
+++ queries/aura/textobjects.scm
@@ -2,13 +2,13 @@
 (fn_decl) @function.outer
 
 (fn_decl
-  body: (block) @function.inner)
+  (block) @function.inner)
 
 ; Macro objects
 (macro_decl) @class.outer
 
 (macro_decl
-  body: (block) @class.inner)
+  (block) @class.inner)
 
 ; Parameter objects
 (parameter) @parameter.outer
